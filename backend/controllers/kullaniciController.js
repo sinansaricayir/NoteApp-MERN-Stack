4bfb93ecd3da4c603bb6fa029b635c1203aed7c3
Sinan Sarıçayır <sinansaricayir@gmail.com>
2022-07-08 15:18:47 +0300

    register-bcyrpt

--- backend/controllers/kullaniciController.js
+++ backend/controllers/kullaniciController.js
@@ -1,7 +1,59 @@
+const asyncHandler = require('express-async-handler')
+const Kullanici = require('../models/kullaniciModel')
+const bcyrpt = require('bcryptjs')
 
-const registerKullanici = (req,res)=>{
-    res.status(200).json({mesaj:"Register işlemi tamamlandı !"})
-}
+const registerKullanici = asyncHandler(async (req,res)=>{
+    const {kullaniciAd,email,parola} = req.body;
+
+    if(!kullaniciAd || !email|| !parola){
+        res.status(400)
+        throw new Error('Lütfen gerekli alanları doldurunuz !')
+    }
+
+    const kullanici = await Kullanici.findOne({email})
+
+    if(kullanici){
+        res.status(400)
+        throw new Error('Bu email mevcut!')
+    }
+
+    const adKontrol = await Kullanici.findOne({kullaniciAd})
+
+    if(adKontrol){
+        res.status(400)
+        throw new Error('Bu kullanıcı adı mevcut !')
+    }
+
+    //Bcyrpt çağırılarak parola şifrelenir
+    const salt = await bcyrpt.genSalt(10)
+    const sifrelenmisParola = await bcyrpt.hash(parola,salt)
+
+   
+    const yeniKullanici = await Kullanici.create({
+         kullaniciAd,
+         email,
+         parola:sifrelenmisParola
+    })
+ 
+
+    if(yeniKullanici){
+        res.status(201).json({
+            _id:yeniKullanici.id,
+            kullaniciAd:yeniKullanici.kullaniciAd,
+            email:yeniKullanici.email
+        })
+    }else{
+        res.status(400)
+        throw new Error('Geçersiz kullanıcı verisi')
+    }
+
+
+    res.json({mesaj:"Register işlemi tamamlandı !"})
+
+})
+
+
+
 
 const loginKullanici = (req,res)=>{
     res.status(200).json({mesaj:"Login işlemi tamamlandı !"})
