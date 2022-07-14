1435155630ca06a5008d3e8333e846af86890575
Sinan Sarıçayır <sinansaricayir@gmail.com>
2022-07-14 16:25:39 +0300

    Register-Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {FaUserCheck} from 'react-icons/fa'
 
 function Login() {
+
+
+const [formData,setFormData] = useState({
+
+    email:'',
+    parola:'',
+  })
+
+  const {email,parola} = FormData
+
+  const onChange = (e) => {
+    setFormData((onceki)=>({ 
+      ...onceki,
+      [e.target.name]:e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  } 
+
   return (
-    <div>Login</div>
+    <>
+
+    <section className='heading'>
+      <h1>
+        <FaUserCheck/> Üye Girişi
+      </h1>
+    </section>
+
+    <section className='form'>
+
+      <form onSubmit={onSubmit} >
+
+        <div className='form-group'>
+        <input type='email' className='form-control' id='email' name ='email' value={email} placeholder ='E-mail Giriniz' onChange={onChange} />
+        </div>
+
+        <div className='form-group'>
+        <input type='password' className='form-control' id='parola' name ='parola' value={parola} placeholder ='Parolanızı Giriniz' onChange={onChange} />
+        </div>
+
+        <div className='form-group'>
+          <button type='submit' className='btn btn-block'>Giriş Yap</button>
+        </div>
+
+      </form>
+
+    </section>
+
+    </>
   )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {FaUser} from 'react-icons/fa'
 
 function Register() {
+
+
+const [formData,setFormData] = useState({
+
+    kullaniciAd:'',
+    email:'',
+    parola:'',
+    parolaKontrol:''
+  })
+
+  const {kullaniciAd,email,parola,parolaKontrol} = FormData
+
+  const onChange = (e) => {
+    setFormData((onceki)=>({ 
+      ...onceki,
+      [e.target.name]:e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  } 
+
   return (
-    <div>Register</div>
+    <>
+
+    <section className='heading'>
+      <h1>
+        <FaUser/> Üyelik Oluştur
+      </h1>
+    </section>
+
+    <section className='form'>
+
+      <form onSubmit={onSubmit} >
+
+        <div className='form-group'>
+        <input type='text' className='form-control' id='kullaniciAd' name ='kullaniciAd' value={kullaniciAd} placeholder ='Kullanıcı Adı Giriniz' onChange={onChange} />
+        </div>
+
+        <div className='form-group'>
+        <input type='email' className='form-control' id='email' name ='email' value={email} placeholder ='E-mail Giriniz' onChange={onChange} />
+        </div>
+
+        <div className='form-group'>
+        <input type='password' className='form-control' id='parola' name ='parola' value={parola} placeholder ='Parola Giriniz' onChange={onChange} />
+        </div>
+
+        <div className='form-group'>
+        <input type='password' className='form-control' id='parolaKontrol' name ='parolaKontrol' value={parolaKontrol} placeholder ='Parola Tekrarını Giriniz' onChange={onChange} />
+        </div>
+
+        <div className='form-group'>
+          <button type='submit' className='btn btn-block'>Üye Ol</button>
+        </div>
+
+      </form>
+
+    </section>
+
+    </>
   )
 }
 
